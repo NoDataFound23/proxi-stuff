9d351d3b718a101f56d5f05e9d18ff9401ebfe95
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-08-21 16:30:27 -0400

    Update CreateMoveExSpeedup.lua
    fix tabbing (wtf github)

--- CreateMoveExSpeedup.lua
+++ CreateMoveExSpeedup.lua
@@ -5,15 +5,15 @@
 local WasCalled = false
 
 hook.Add("CreateMove", "_", function(cmd)
-  WasCalled = true
-    hook.Run("CreateMoveEx", cmd)
-  WasCalled = false
+	WasCalled = true
+		hook.Run("CreateMoveEx", cmd)
+	WasCalled = false
 end)
 
 hook.Add("CreateMoveEx", "_", function(cmd)
-  if WasCalled then
-    -- Run whatever needed to be sped up in here
-  else
-    -- Run regular logic in here + return for bSendPacket in here 
-  end
+	if WasCalled then
+		-- Run whatever needed to be sped up in here
+	else
+		-- Run regular logic in here + return for bSendPacket in here 
+	end
 end)
