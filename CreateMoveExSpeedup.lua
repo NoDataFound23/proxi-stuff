6c5b9c705b223878b172bd066a4ce452b6c6f842
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-08-21 16:29:40 -0400

    Create CreateMoveExSpeedup.lua

---
+++ CreateMoveExSpeedup.lua
@@ -0,0 +1,19 @@
+--[[
+	https://github.com/leme6156/proxi-stuff
+]]
+
+local WasCalled = false
+
+hook.Add("CreateMove", "_", function(cmd)
+  WasCalled = true
+    hook.Run("CreateMoveEx", cmd)
+  WasCalled = false
+end)
+
+hook.Add("CreateMoveEx", "_", function(cmd)
+  if WasCalled then
+    -- Run whatever needed to be sped up in here
+  else
+    -- Run regular logic in here + return for bSendPacket in here 
+  end
+end)
